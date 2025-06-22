ac5fa528fd43d4131252dc7cbb795b75bbc823f7
s-kiura <s-kimura@safie.jp>
2025-06-23 05:25:27 +0900

    Fix scheduler time

--- backend/searcher/scheduler.js
+++ backend/searcher/scheduler.js
@@ -4,17 +4,17 @@ import Paper from "../models/Paper.js";
 import Post from "../models/Post.js";
 
 import extractPdfText from "./pdfParser.js";
-import {searchPapers} from "./searchScholar.js";
+import { searchPapers } from "./searchScholar.js";
 import summarizer from "./summarizer.js";
 
 // スケジュールタスクを設定
 function initializeScheduler() {
-    schedule.scheduleJob('00 09 * * 1-5', async () => { // 月~金の0時に投稿
+    schedule.scheduleJob('00 09 * * 1-5', async () => { // 月~金の9時に投稿
         console.log('Bot is running to post papers');
 
         try {
             const query = 'Computer vision';                            // 検索クエリ
-            const postingPaperNum = 1;                                  // 一度に投稿する論文の数
+            const postingPaperNum = 5;                                  // 一度に投稿する論文の数
             const results = await searchPapers(query, postingPaperNum); // 論文を検索
 
             for (const paper of results) {
@@ -25,16 +25,20 @@ function initializeScheduler() {
 
                     // 検索した論文の要約をボットの Post としてデータベースに保存
                     const newPost = new Post({
-                        userId : '6803c93270fbdf7e2ea0bcc7', // ボットアカウントのユーザーID
-                        desc : `${summary}`,
-                        createdAt : new Date(),
-                        paperId : paper.paperId
+                        userId: '68568248fc5027153a504774', // ボットアカウントのユーザーID
+                        desc: `${summary}`,
+                        createdAt: new Date(),
+                        paperId: paper.paperId
                     });
 
                     await newPost.save();
 
                     // 検索した論文を Paper のデータベースに保存
-                    await Paper.findOneAndUpdate({paperId : paper.paperId}, paper, {upsert : true, new : true});
+                    await Paper.findOneAndUpdate(
+                        { paperId: paper.paperId },
+                        { $set: paper },
+                        { upsert: true, new: true }
+                    );
 
                 } catch (error) {
                     console.error(`Error occured in summarizeing paper "${paper.title}"\n`, error);
